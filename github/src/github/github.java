8ba9e8e84d057b949d94e62250f20463633c08fa
VaqueveraMac <VaqueveraMac@192.168.1.36>
2020-02-15 09:40:18 +0100

    prueba github

---
+++ github/src/github/github.java
@@ -0,0 +1,24 @@
+package github;
+
+public class github {
+
+	public static void main(String[] args) {
+		int repeticiones = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j < 10; j++) {
+
+				repeticiones++;
+
+				System.out.println("Hola alumnos " + repeticiones);
+
+			}
+
+		}
+
+		System.out.println("Has salido del bucle");
+
+	}
+
+}
